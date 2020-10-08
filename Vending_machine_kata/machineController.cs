7bd2b72473d8ee57f3f773ee8a85b9c18cd98c39
baginski95 <bagiczny3@gmail.com>
2020-10-08 19:47:13 +0200

    Created func that add Coins when machine starts working

--- Vending_machine_kata/machineController.cs
+++ Vending_machine_kata/machineController.cs
@@ -6,6 +6,27 @@ namespace Vending_machine_kata
 {
     class MachineController
     {
-        public VendingMachine 
+        public VendingMachine VendingMachine { get; }
+        public MachineController()
+        {
+
+        }
+        private Dictionary<Coin,int> InsertStartingCoins()
+        {
+            var startingCoins = new Dictionary<Coin, int>()
+            {
+                { Coin.FiveZL, 10 },
+                { Coin.TwoZL, 10 },
+                { Coin.OneZL, 10 },
+                { Coin.FiftyGR, 10 },
+                { Coin.TwentyGR, 10 },
+                { Coin.TenGR, 10 },
+            };
+            return startingCoins;
+        }
+        private Dictionary<int,Product> InsertStartingProducts()
+        {
+
+        }
     }
 }
